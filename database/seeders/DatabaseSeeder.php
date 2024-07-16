e4509ae3892a0f4e2fa75def2503e7083b3ad773
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2024-07-16 12:09:12 +0800

    task migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(50)->create();
+        \App\Models\User::factory(50)->create();
         \App\Models\Task::factory(500)->create();
 
         // \App\Models\User::factory()->create([

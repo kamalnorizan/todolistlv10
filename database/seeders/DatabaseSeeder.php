760be0fc6d607c5a0eb2a6edcab1385baafb4712
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2024-07-16 11:45:56 +0800

    migration tasks

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(50)->create();
+        // \App\Models\User::factory(50)->create();
+        \App\Models\Task::factory(500)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',

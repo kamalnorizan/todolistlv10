328dcd2fb466e3b61b0397f2213e42c0830731a8
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2024-07-18 16:52:55 +0800

    create task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use App\Models\User;
+use Ramsey\Uuid\Uuid;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 
@@ -47,10 +49,6 @@ class TaskController extends Controller
         return view('tasks.show', compact('task'));
     }
 
-    function store(Request $request)  {
-        dd($request);
-    }
-
     function ajaxloadtasks(Request $request) {
         $tasks = Task::with('user');
 
@@ -70,5 +68,36 @@ class TaskController extends Controller
         })
         ->rawColumns(['action'])
         ->make(true);
+    }
+
+    function create() {
+        $users = User::pluck('name','id');
+        return view('tasks.create', compact('users'));
+    }
+
+    function store(Request $request) {
+        $request->validate([
+            "title" => 'required|max:255',
+            "user_id" => 'required',
+            "due_date" => 'required|date|after_or_equal:today',
+            "description" => 'required'
+        ],[
+            'title.required' => 'Sila masukkan tajuk',
+            'user_id.required' => 'Sila pilih user',
+            'due_date.required' => 'Sila pilih tarikh',
+            'due_date.after_or_equal' => 'Tarikh mesti selepas hari ini',
+            'due_date.date' => 'Sila pilih tarikh',
+            'description.required' => 'Sila masukkan description'
+        ]);
+
+        $task = new Task();
+        $task->uuid = Uuid::uuid4();
+        $task->title = $request->title;
+        $task->user_id = $request->user_id;
+        $task->due_date = $request->due_date;
+        $task->description = $request->description;
+        $task->save();
+
+        return redirect()->route('tasks.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,8 @@ Route::get('/users/{id}/edit', [UserController::class, 'edit']);
 
 Route::get('tasks',[TaskController::class,'index'])->name('tasks.index');
 Route::post('tasks/ajaxloadtasks',[TaskController::class,'ajaxloadtasks'])->name('tasks.ajaxloadtasks');
+Route::get('tasks/create', [TaskController::class,'create'])->name('tasks.create');
+Route::post('tasks', [TaskController::class,'store'])->name('tasks.store');
 Route::get('tasks/{task}',[TaskController::class,'show'])->name('tasks.show');
 
 Auth::routes();
